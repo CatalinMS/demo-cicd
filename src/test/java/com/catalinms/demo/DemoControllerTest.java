8de0f24d4c85aead7c36bca6a8d2725878a756e0
Catalin Moldovan <catalin.moldovan@globant.com>
2020-02-14 16:23:36 +0200

    Add dummy test

---
+++ src/test/java/com/catalinms/demo/DemoControllerTest.java
@@ -0,0 +1,14 @@
+package com.catalinms.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoControllerTest {
+
+    @Test
+    public void demoTest() {
+        assertEquals(2, 2);
+    }
+
+}
